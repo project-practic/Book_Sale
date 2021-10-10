2a6ff17bcef6ece4bb517863b521bdd30bca2715
VusalNecefov <vusal.nacafov@kapitalbank.az>
2021-10-10 12:40:51 +0400

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar ./build/libs/Book_Sale-0.0.1-SNAPSHOT.war
---
+++ src/main/java/com/booksale/booksale/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.booksale.booksale;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BookSaleApplication.class);
+    }
+
+}
