7a32d8e5f43eb316f19fb2ce9d0d0ef14860ffe2
VusalNecefov <vusal.nacafov@kapitalbank.az>
2021-10-10 12:56:18 +0400

    Enable Swagger

---
+++ src/main/java/com/booksale/booksale/config/SwaggerConfiguration.java
@@ -0,0 +1,48 @@
+package com.booksale.booksale.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+
+
+    @Value("${application.swagger.title}")
+    private String apiTitle;
+    @Value("${application.swagger.description}")
+    private String apiDescription;
+    @Value("${application.swagger.version}")
+    private String apiVersion;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.booksale.booksale"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo())
+                .produces(Collections.singleton(MediaType.APPLICATION_JSON_VALUE))
+                .consumes(Collections.singleton(MediaType.APPLICATION_JSON_VALUE));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(apiTitle)
+                .version(apiVersion)
+                .description(apiDescription)
+                .build();
+    }
+}
